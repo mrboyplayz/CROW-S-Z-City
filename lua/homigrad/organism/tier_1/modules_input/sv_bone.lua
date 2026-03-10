b86db20d7be8f29c325080c973dc8dc77921855d
Jrfhj Guhvf <jrfhjguhvf@gmail.com>
2026-03-10 18:18:48 +0300

    aaaaaaa
    DEAR LORD IM IN A RUSH

--- lua/homigrad/organism/tier_1/modules_input/sv_bone.lua
+++ lua/homigrad/organism/tier_1/modules_input/sv_bone.lua
@@ -212,7 +212,9 @@ local function spine(org, bone, dmg, dmgInfo, number, boneindex, dir, hit, ricoc
 
 	if org[name] >= hg.organism[name2] and org.isPly then
 		org.owner:EmitSound("bones/bone"..math.random(8)..".mp3", 75, 100, 1, CHAN_AUTO)
-		org.owner:Notify(huyasd[name], true, name, 2)
+		if org.owner:IsPlayer() then
+			org.owner:Notify(huyasd[name], true, name, 2)
+		end
 		org.painadd = org.painadd + 25
 	end
 	
@@ -320,10 +322,11 @@ input_list.skull = function(org, bone, dmg, dmgInfo, boneindex, dir, hit, ricoch
 		timer.Simple(0.1, function()
 			local rag = hg.GetCurrentCharacter(org.owner)
 
-			if rag:IsRagdoll() then
-				local stype = hg.getRandomSpasm()
-				hg.applySpasm(rag, stype)
-				if rag.organism then rag.organism.spasm, rag.organism.spasmType = true, stype end
+			if IsValid(rag) and rag:IsRagdoll() then
+				hg.applyFencingToPlayer(org.owner, org)
+				--local stype = "rigor"--hg.getRandomSpasm()
+				--hg.applySpasm(rag, stype)
+				--if rag.organism then rag.organism.spasm, rag.organism.spasmType = true, stype end
 			end
 		end)
 	end
